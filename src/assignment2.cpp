5b1d9a2080ec8598cfbd67b53ca9c7fae66cfcc3
jdcoder75 <89999560+jdcoder75@users.noreply.github.com>
2021-11-29 20:31:49 -0500

    Add files via upload

---
+++ src/assignment2.cpp
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Jose Malave
+ */
+//ex02
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+  char x;
+  double y, z;
+  cin >> x; // Get user input from the keyboard
+  cin>> y; 
+  cin>> z;
+
+  cout << x << " " << y << " " << z;
+  return 0;
+}
